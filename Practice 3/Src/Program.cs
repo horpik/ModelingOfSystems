922dedb0494b72ee23e8a98ed1f2a440949b4df4
Daniil <horpuru@gmail.com>
2023-11-12 18:18:01 +0300

    Create func generate graphs normal distribution

--- Practice 3/Src/Program.cs
+++ Practice 3/Src/Program.cs
@@ -6,19 +6,30 @@ namespace Practice_3
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            double mean = 0;
-            double stddev = 1;
+            double[] mean = { 10, 10, 10, 12 };
+            double[] stddev = { 2, 1, 0.5, 1 };
             int numPoints = 1000;
-            var x = Generate.LinearSpaced(numPoints, -3 * stddev, 3 * stddev);
-            var y = x.Select(val => NormalDistribution.PDF(val, mean, stddev)).ToArray();
+            CreateGraphsNormalDistribution(mean, stddev, numPoints);
+        }
 
+        private static void CreateGraphsNormalDistribution(double[] mean, double[] stddev, int numPoints)
+        {
+            for (int i = 0; i < mean.Length; i++)
+            {
+                var x = Generate.LinearSpaced(numPoints, -3 * stddev[i], 3 * stddev[i]);
+                var y = x.Select(val => NormalDistribution.PDF(val, mean[i], stddev[i])).ToArray();
+                GeneratePlot(x, y, $"normal_distribution{i}");
+            }
+        }
+
+        private static void GeneratePlot(double[] x, double[] y, string fileName)
+        {
             var plt = new Plot(600, 400);
             plt.PlotScatter(x, y);
-            plt.SaveFig("normal_distribution.png");
 
-            plt.SaveFig("D:\\Dev\\Projects\\C#\\ModelingOfSystems\\Practice 3\\Graphics\\normal_distribution.png");
+            plt.SaveFig($"..\\..\\Graphics\\{fileName}.png");
         }
     }
 }
