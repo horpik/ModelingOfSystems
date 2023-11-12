59cd7f7143bd2a2ce1249f25211cfd1e8de1753a
Daniil <horpuru@gmail.com>
2023-11-12 18:29:06 +0300

    Add task 2

--- Practice 3/Src/Program.cs
+++ Practice 3/Src/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using ScottPlot;
 using Generate = MathNet.Numerics.Generate;
 
@@ -10,6 +11,8 @@ namespace Practice_3
         {
             double[] mean = { 10, 10, 10, 12 };
             double[] stddev = { 2, 1, 0.5, 1 };
+            // double[] mean = { 10 };
+            // double[] stddev = { 2 };
             int numPoints = 1000;
             CreateGraphsNormalDistribution(mean, stddev, numPoints);
         }
@@ -18,8 +21,12 @@ namespace Practice_3
         {
             for (int i = 0; i < mean.Length; i++)
             {
-                var x = Generate.LinearSpaced(numPoints, -3 * stddev[i], 3 * stddev[i]);
+                var x = Generate.LinearSpaced(numPoints, -20 * stddev[i], 20 * stddev[i]);
                 var y = x.Select(val => NormalDistribution.PDF(val, mean[i], stddev[i])).ToArray();
+                
+                // var x = new double[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+                // var y = x.Select(val => NormalDistribution.PDF(val, mean[i], stddev[i])).ToArray();
+                
                 GeneratePlot(x, y, $"normal_distribution{i}");
             }
         }
