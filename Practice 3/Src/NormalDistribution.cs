8597259a5d3cdce38c26dd4e17da49ffc2e36c85
Daniil <horpuru@gmail.com>
2023-11-12 17:17:20 +0300

    Add first task

---
+++ Practice 3/Src/NormalDistribution.cs
@@ -0,0 +1,13 @@
+﻿using MathNet.Numerics.Distributions;
+
+namespace Practice_3
+{
+    public static class NormalDistribution
+    {
+        public static double PDF(double x, double mean, double stddev)
+        {
+            var normal = new Normal(mean, stddev);
+            return normal.Density(x);
+        }
+    }
+}
---
+++ Practice 3/Src/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using ScottPlot;
+using Generate = MathNet.Numerics.Generate;
+
+namespace Practice_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double mean = 0;
+            double stddev = 1;
+            int numPoints = 1000;
+            var x = Generate.LinearSpaced(numPoints, -3 * stddev, 3 * stddev);
+            var y = x.Select(val => NormalDistribution.PDF(val, mean, stddev)).ToArray();
+
+            var plt = new Plot(600, 400);
+            plt.PlotScatter(x, y);
+            plt.SaveFig("normal_distribution.png");
+
+            plt.SaveFig("D:\\Dev\\Projects\\C#\\ModelingOfSystems\\Practice 3\\Graphics\\normal_distribution.png");
+        }
+    }
+}
